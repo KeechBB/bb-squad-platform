7d50a133d819b2268b6b825abf7325b23d34fa39
KeechBB <keech@users.noreply.github.com>
2026-09-19 13:35:23 +0300

    Remove opaque backdrop behind transparent clan logos.

--- src/app/cw/page.tsx
+++ src/app/cw/page.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 /** Меняй при обновлении KV, чтобы iframe не брал старый кэш */
-const KV_CACHE = "20260919-colfilters";
+const KV_CACHE = "20260919-ratingfix";
 
 export default async function CwPage({ searchParams }: Props) {
   const sp = searchParams ? await searchParams : {};
